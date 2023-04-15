70fa012c11b40ce637f9c578127c152e7fe09515
Subhradip Paul <subhradip.paul.sp@gmail.com>
2023-04-15 16:12:56 +0200

    updated KBr temp script

--- temp_from_KBr.py
+++ temp_from_KBr.py
@@ -24,28 +24,29 @@ def main():
                 '$$T_1 = 0.0145 + 5330 \\times T^{-2} + 1.42e7 \\times T^{-4} + 2.48e9 \\times T^{-6}$$ <br>' +
                 '[Thurber, Tycko. JMR 196, no. 1: 84–87](https://doi.org/10.1016/j.jmr.2008.09.019).' 
                 , unsafe_allow_html=True)
-    temp = np.linspace(20,400,4000)
-    y0=0.0145*np.ones(np.size(temp));
-    y1=5330*(temp ** -2);
-    y2=1.42e7*(temp ** -4);
-    y3=2.48e9*(temp ** -6);
-    y=y0+y1+y2+y3;
+    temp = np.linspace(15,400,10000)
+    y0=0.0145*np.ones(np.size(temp))
+    y1=5330*(temp ** -2)
+    y2=1.42e7*(temp ** -4)
+    y3=2.48e9*(temp ** -6)
+    y=y0+y1+y2+y3
     
 
     #%% Enter the relaxation time and find the temperature
     t1 = st.number_input("Enter a relaxation time in s: ", value = 15)
-    k, l=find_nearest(y, t1)
+    _, l=find_nearest(y, t1)
 
 
     #%% Create the figure
     fig, ax = plt.subplots()
-    ax.plot(temp,y)
-    ax.plot((temp[1], temp[np.size(temp)-1]), (t1, t1), 'k--')
+    ax.plot(temp,np.log10(y))
+    ax.plot((temp[1], temp[np.size(temp)-1]), (np.log10(t1), np.log10(t1)), 'k--', label=str(np.round(temp[l],2)))
     ax.set_xlabel('Relaxation Time (s)')
-    ax.set_ylabel('Temperature (K)')
+    ax.set_ylabel('log10 (T) (K)')
+    ax.legend(loc='upper right')
 
     #%% Output cells
-    st.write('The temperature is '+ str(np.round(temp[l],2)) + ' K')
+    st.write('#### The temperature is '+ str(np.round(temp[l],2)) + ' K')
     st.pyplot(fig)
 
 if __name__ == "__main__":
