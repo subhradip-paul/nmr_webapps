a3ed09a55e3ef9483dee8508244591d7509b493d
Subhradip Paul <subhradip.paul.sp@gmail.com>
2023-04-16 13:21:08 +0200

    added script to calculate max sensitivity

---
+++ max_sensitivity_fromt1.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import Model
+from sympy import symbols, diff, exp, sqrt, lambdify
+import streamlit as st
+
+def optimumrecycledelaytwoexp(x, ampl1, tau1, tau2):
+    t, b, a, Ta, Tb = symbols ('t b a Ta Tb')
+    b = 1-a
+    s=a*(1-exp(-t/Ta))/sqrt(t)+b*(1-exp(-t/Tb))/sqrt(t)
+    bup=s*sqrt(t)
+    k=diff(s,t)
+    y_bup=bup.subs({Ta:tau1,Tb:tau2,a:ampl1})
+    y=k.subs({Ta:tau1,Tb:tau2,a:ampl1})
+    lam_x = lambdify(t, y, modules=['numpy'])
+    lam_x2 = lambdify(t, y_bup, modules=['numpy'])
+    x_vals = np.linspace(0.1, x, 100000)
+    y_vals = lam_x(x_vals)
+    y_vals_bup = lam_x2(x_vals)
+    return x_vals, y_vals_bup, x_vals[np.argmin(abs(y_vals))]
+
+def optimumrecycledelayoneexp(x, tau1):
+    t, Ta = symbols ('t Ta')
+    s=(1-exp(-t/Ta))/sqrt(t)
+    bup=s*sqrt(t)
+    k=diff(s,t)
+    y_bup=bup.subs({Ta:tau1})
+    y=k.subs({Ta:tau1})
+    lam_x = lambdify(t, y, modules=['numpy'])
+    lam_x2 = lambdify(t, y_bup, modules=['numpy'])
+    x_vals = np.linspace(0.1, x, 100000)
+    y_vals = lam_x(x_vals)
+    y_vals_bup = lam_x2(x_vals)
+    return x_vals, y_vals_bup, x_vals[np.argmin(abs(y_vals))]
+
+def optimumrecycledelaystrexp(x, tau1, beta):
+    t, b, Ta = symbols ('t b Ta')
+    s=(1-exp(-np.power((t/Ta),b)))/sqrt(t)
+    bup=s*sqrt(t)
+    k=diff(s,t)
+    y_bup=bup.subs({Ta:tau1,b:beta})
+    y=k.subs({Ta:tau1,b:beta})
+    lam_x = lambdify(t, y, modules=['numpy'])
+    lam_x2 = lambdify(t, y_bup, modules=['numpy'])
+    x_vals = np.linspace(0.1, x, 100000)
+    y_vals = lam_x(x_vals)
+    y_vals_bup = lam_x2(x_vals)
+    return x_vals, y_vals_bup, x_vals[np.argmin(abs(y_vals))]
+
+option = st.selectbox('Which model?', ('Monoexponential', 'Biexponential', 'Stretched Exponential'))
+
+
+if option == 'Monoexponential':
+    tb = st.number_input('Enter the time component: ', value = 10.0)
+    max_time = st.number_input('Enter the maximum time in the buildup: ', value = 512.0)
+    x, y, optd1 = optimumrecycledelayoneexp(max_time, tb)
+
+if option == 'Stretched Exponential':
+    tb = st.number_input('Enter the time component: ', value = 10.0)
+    beta = st.number_input('Enter the time component: ', min_value=0.0, max_value=1.0, step=0.1, value = 0.5)
+    max_time = st.number_input('Enter the maximum time in the buildup: ', value = 512.0)
+    x, y, optd1 = optimumrecycledelaystrexp(max_time, tb, beta)
+
+if option == 'Biexponential':
+    ta = st.number_input('Enter the 1st time component: ', value = 1.0)
+    tb = st.number_input('Enter the 2nd time component: ', value = 10.0)
+    comp1 = st.number_input('Enter the Amplitude 1: ', value = 1.0)
+    comp2 = st.number_input('Enter the Amplitude 1: ', value = 99.0)
+    amp1=comp1/(comp1+comp2)
+    max_time = st.number_input('Enter the maximum time in the buildup: ', value = 512.0)
+    x, y, optd1 = optimumrecycledelaytwoexp(max_time, amp1, ta, tb)
+    
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.plot((optd1,optd1),(0.0,1.0),'k--')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Intensity')
+
+st.write('The optimum recycle delay is ' + str(np.round(optd1,2)) + ' s')
+st.pyplot(fig)
