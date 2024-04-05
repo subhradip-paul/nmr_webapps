f3527c2bde2b15889e7ddcb21d18043199e9d57d
Subhradip Paul <subhradip.paul.sp@gmail.com>
2024-04-05 11:26:50 +0200

    updated dipole to distance script

--- src/dipole_calculator.py
+++ src/dipole_calculator.py
@@ -38,6 +38,8 @@ def dipole2dist(choice_nuc1, choice_nuc2, dipole):
     
 
 if __name__ == "__main__":
+    st.latex(r"\text{Dipole} \leftrightarrow \text{Distance}")
+    st.divider()
     choice_nuc1 = st.selectbox('Nucleus 1:', nuctable.Name.values.tolist(), index=2)
     choice_nuc2 = st.selectbox('Nucleus 2:', nuctable.Name.values.tolist(), index=11)
     choice_calc = st.radio('Method', ('Distance to Dipole', 'Dipole to Distance'))
@@ -45,10 +47,12 @@ if __name__ == "__main__":
     if choice_calc == 'Distance to Dipole':
         distance=st.number_input('Enter distance in Angstrom: ', min_value=0.5, value=1.07)
         coup = dist2dipole(choice_nuc1,choice_nuc2,distance)
+        st.divider()
         st.write("The dipolar coupling = " + str(np.abs(np.round(coup,2))) + ' Hz')
         st.write("The dipolar coupling = " + str(np.abs(np.round(coup/1e3,2))) + ' kHz')
     else:
         dipole=st.number_input('Enter dipolar coupling in Hz: ', min_value=0.5)
         d=dipole2dist(choice_nuc1,choice_nuc2,dipole)
-        st.write("The distance  = " + str(np.abs(np.round(d,2))) + ' A')
+        st.divider()
+        st.latex(r"\text{Distance  = }" + str(np.abs(np.round(d,2))) + r'\ {\AA}')
         
