89f0fdf7676fe73d7a8ccaa68e9f83b4614a9637
Subhradip Paul <subhradip.paul.sp@gmail.com>
2024-04-16 17:13:31 +0200

    updated home

--- homepage.py
+++ homepage.py
@@ -1,11 +1,8 @@
 import streamlit as st
+from st_pages import Page, Section, show_pages, add_indentation
 
-st.set_page_config(
-    page_title="Python apps for NMR",
-    page_icon=":atom_symbol:",
-)
+st.image('dnp_grenoble_logo.png')
 
-st.title("Python apps for NMR")
 st.markdown("Some small apps written by me to help NMR calculations and setting up experiments.\
     The apps are written in [Python](https://www.python.org/) and deployed using [Streamlit](https://streamlit.io/).\
         If you want to help or have suggestions, or if you spot some mistakes, please let me know on the github repository.\
@@ -15,4 +12,17 @@ with col1:
     st.image('streamlit-mark-color.png', width=40)
 with col2:
     st.image('Python-logo.png', width=40)
-st.sidebar.success("Select an app above")
+
+add_indentation()
+show_pages(
+    [   
+        Page("homepage.py", "Home", ":house:"),
+        Page("pages/nmr_properties.py", "NMR Properties of Nuclei", ":atom_symbol:"),
+        Section("Laboratory Stuff", icon="🥼"),
+        Page("pages/dnp_sample_prep.py", "DNP Sample Prep"),
+        Section("Experiments", icon="🧨"),
+        Page("pages/inadequate_efficiency.py", "INADEQUATE Efficiency"),
+        Page("pages/optimum_sensitivity.py", "Optimum Sensitivity based on T1"),
+        Page("pages/temp_from_KBr.py", "Temperature from KBr")
+    ]
+)
